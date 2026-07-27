ce2dcba1e034fd672f9e2aebdf58a383082874ce
Ryan Gurney <ryangurney24@gmail.com>
2026-07-26 20:21:03 -0400

    Concat role and transcript + new line delimiter

--- analyze.sql
+++ analyze.sql
@@ -1,6 +1,6 @@
 -- List all tables in the current SQLite database
 SELECT
   thread_id,
-  GROUP_CONCAT(role || ': ' || content, ' /n/n ') AS conversation
+  GROUP_CONCAT(role || ': ' || content, char(10) || char(10)) AS conversation
 FROM messages
 GROUP BY thread_id;
